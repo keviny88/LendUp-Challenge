454f854abc83c291d096ea3ad132d8d86a0cd8dd
Kevin Yin <keviny.whs@gmail.com>
2017-12-26 17:50:55 -0800

    Testing input

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 	
 	//Create a route that will handle Twilio webhook requests
-	require_once __DIR__.'\vendor\autoload.php';
-	use Twilio\Twiml;
+	echo "HELLLO";
+	// require_once __DIR__.'\vendor\autoload.php';
+	// use Twilio\Twiml;
 
 
 	//Use Twilio PHP SDK to build an XML response
