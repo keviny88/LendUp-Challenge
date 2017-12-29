e272211d3227463171e257d2f3b5f72ad19a99ad
Kevin Yin <keviny.whs@gmail.com>
2017-12-28 16:20:07 -0800

    Phase 2 in Progress, added calling functionality

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>The Ultimate LendUp Challenge</title>
+	<meta name="description" content="The HTML5 Herald">
+	<meta name="Kevin" content="SitePoint">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+</head>
+
+<body>
+
+	<div class="container" style="padding-top: 150px">
+		<h1 class="display-1" style="text-align: center;">FizzBuzz</h1>
+		<form>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Phone Number</label>
+		    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter number">
+		    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		  </div>
+		  <button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
